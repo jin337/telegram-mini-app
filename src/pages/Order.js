b1cb49e130e29a6e8c89296921aa1f252e58440e
jin <jin337x@outlook.com>
2024-02-03 23:17:08 +0800

    update

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -20,7 +20,7 @@ function Order({ list, handleEdit }) {
 
   return (
     <main className='w-screen h-screen bg-neutral-200'>
-      <div className='flex justify-between bg-white pt-10 px-4'>
+      <div className='flex justify-between bg-white pt-8 px-4'>
         <div className='text-base font-bold'>YOUR ORDER</div>
         <div
           className='text-sm text-green-500 cursor-pointer'
@@ -41,10 +41,10 @@ function Order({ list, handleEdit }) {
         <input
           type='text'
           placeholder='Add comment'
-          className='text-xl w-full my-4 px-4 py-2 focus-visible:outline-none'
+          className='text-base w-full my-4 px-4 py-2 focus-visible:outline-none'
         />
       </div>
-      <div className='text-base text-neutral-400 px-4 bg-neutral-200 pb-10'>Any special requests,details,final wishes etc.</div>
+      <div className='text-sm text-neutral-400 px-4 bg-neutral-200 pb-10'>Any special requests,details,final wishes etc.</div>
     </main>
   )
 }
