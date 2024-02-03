e9944c43c26cd3ae4a45e51ccf819004b3fc2db4
jin <jin337x@outlook.com>
2024-02-03 23:20:44 +0800

    update

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -41,7 +41,7 @@ function Order({ list, handleEdit }) {
         <input
           type='text'
           placeholder='Add comment'
-          className='text-base w-full my-4 px-4 py-2 focus-visible:outline-none'
+          className='text-base w-full my-3 px-4 py-2 focus-visible:outline-none'
         />
       </div>
       <div className='text-sm text-neutral-500 px-4 bg-neutral-100 pb-10'>Any special requests,details,final wishes etc.</div>
