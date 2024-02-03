fa3811d26d438af3ed19ec674468bed6f7ec6e3a
jin <jin337x@outlook.com>
2024-02-04 00:12:43 +0800

    update

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Cart({ item }) {
   return (
-    <div className='px-4 py-3 flex justify-between'>
+    <div className='px-4 py-2 flex justify-between'>
       <div className='flex'>
         <div className='text-4xl'>{item.Image}</div>
         <div className='ml-2'>
