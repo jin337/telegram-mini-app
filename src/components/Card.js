5be3f605c0de7d530a114502726ae4007f31aa36
jin <jin337x@outlook.com>
2024-02-04 15:16:29 +0800

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,10 +13,10 @@ function Card({ item, onAdd, onRemove }) {
   }
 
   return (
-    <div className="relative w-28 text-center my-4 z-0">
+    <div className="relative w-28 text-center my-3 z-0">
       <div className={`text-xs leading-none font-bold text-white bg-amber-500 size-6 flex items-center justify-center rounded-full absolute right-3 ${count !== 0 ? 'visible' : 'hidden'}`}>{count}</div>
-      <div className='text-6xl'>{item.Image}</div>
-      <div className='text-sm my-2 text-nowrap'>{item.title} · <span className="font-bold">$ {item.price}</span></div>
+      <div className='text-6xl my-2'>{item.Image}</div>
+      <div className='text-sm mb-2 text-nowrap'>{item.title} · <span className="font-bold">$ {item.price}</span></div>
       <div className='flex gap-1.5 px-4'>
         {count !== 0 && <Button title={'-'} type={'remove'} onClick={handleDecrement} />}
         <Button title={count === 0 ? 'ADD' : '+'} type={'add'} onClick={handleIncrement} />
