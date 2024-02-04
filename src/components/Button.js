33577fed318a27822ae4ef0d14d99d0af9bc2f65
jin <jin337x@outlook.com>
2024-02-04 16:12:25 +0800

    update

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({ title, type, onClick }) {
   return (
-    <div className={`transition-all duration-300 text-sm leading-none font-bold text-white text-center w-full p-2 box-border rounded-md ${type === 'remove' ? 'bg-red-500' : 'bg-amber-500'} `} onClick={onClick}>{title}</div>
+    <div className={`transition-all duration-300 text-sm leading-none font-bold text-white text-center w-full p-2 box-border rounded-md cursor-pointer ${type === 'remove' ? 'bg-red-500 active:bg-red-600' : 'bg-amber-500 active:bg-amber-600'} `} onClick={onClick}>{title}</div>
   )
 }
 
